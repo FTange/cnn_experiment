9d4069008814a340fd7a99d6592b02588f955a13
Frederik Tange <Frederiktange@gmail.com>
2018-10-28 13:45:48 +1300

    exp final perf 1 layer

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 import sklearn
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, f1_score, precision_recall_fscore_support
 
 import plot
 from read_data import *
@@ -13,30 +13,41 @@ from evaluate import *
 
 
 x, y = get_train_data()
+x_test, y_test = get_test_data()
 # x = x / x.max()
 
 input_dim = x.shape[1] * x.shape[2]
 output_dim = y.max() + 1
 
 
+"""
 x_train, x_test, y_train, y_test = \
     train_test_split(x, y, test_size=0.2, random_state=1)
-
-y_train_nn = transform_y_for_nn(y_train)
+"""
 
 model_layers = [tf.keras.layers.Flatten(),
                 tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(128, activation=tf.nn.relu),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                tf.keras.layers.Dense(2024, activation=tf.nn.relu),
+                tf.keras.layers.BatchNormalization(),
                 tf.keras.layers.Dropout(0.2),
                 tf.keras.layers.Dense(output_dim, activation=tf.nn.softmax)]
 
-accuracy, f1 = repeated_tests(model_layers, x_train, y_train, y_train_nn)
+model = tf.keras.models.Sequential(model_layers)
 
-print('acc: {}, f1: {}'.format(accuracy, f1))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+model.fit(x, y, epochs=1)
+y_pred = model.predict(x_test)
+y_pred = np.apply_along_axis(np.argmax, 1, np.round(y_pred))
 
+p, r, f, _ = precision_recall_fscore_support(y_test, y_pred, average = 'weighted')
+acc = accuracy_score(y_test, y_pred)
+
+print('precision: {:.4f}, recall: {:.4f}, f1: {:.4f}, accuracy: {:.4f}'.format(p, r, f, acc))
+
+# accuracy, f1 = repeated_tests(model_layers, x_train, y_train, y_train_nn)
 
 """
 model = tf.keras.models.Sequential([
